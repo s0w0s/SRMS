b2a37317e5f062c1462e947d47c5f469286b9755
woosung <jangwooseng97@gmail.com>
2022-01-15 18:32:54 +0900

    fix form 에서 edit기능 추가 클릭을 통해서 해당 폼을 넣어주는 기능

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,8 +1,11 @@
 import {
   Component,
+  OnDestroy,
   OnInit,
+  ViewChild,
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -12,17 +15,39 @@ import { ShoppingListService } from '../shopping-list.service';
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  Subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
+
+  @ViewChild('f', {static: false}) slForm: NgForm;
+
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.Subscription = this.slService.startedEditing
+      .subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+          this.editedItem = this.slService.getIngredient(index);
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          })
+        }
+      );
   }
 
   onAddItem(form: NgForm) {
     const value = form.value;
-    console.log(value);
     const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
+  }
+
+  ngOnDestroy() {
+    this.Subscription.unsubscribe();
   }
 
 }
