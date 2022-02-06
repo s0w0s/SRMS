37ec063be86326eae0c2617f27d92c09f854558f
woosung <jangwooeng97@gmail.ocm>
2022-02-06 09:11:00 +0900

    fix(ngrx): 쇼핑 리스트 및 인증 ngrx모듈로 변환

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { LoggingService } from './logging.service';
+import * as fromApp from './store/app.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -15,10 +16,11 @@ import { LoggingService } from './logging.service';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot(fromApp.appReducer),
     SharedModule,
     CoreModule
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
   // providers: [LoggingService]
 })
 export class AppModule {}
