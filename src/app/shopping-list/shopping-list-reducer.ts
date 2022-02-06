f484fa931c48e2859797408bd195ee4029808000
woosung <jangwooeng97@gmail.ocm>
2022-02-06 19:47:54 +0900

    fix(ngrx): 인증효과 종료

--- src/app/shopping-list/shopping-list-reducer.ts
+++ src/app/shopping-list/shopping-list-reducer.ts
@@ -1,7 +1,7 @@
 import { Ingredient } from "../shared/ingredient.model";
 
 const initialState = {
-  ingredients: Ingredient[] = [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 10)];
+  ingredients: Ingredient[] = [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 10)],
 };
 
 export function shoppingListReducer(state, action) {}
